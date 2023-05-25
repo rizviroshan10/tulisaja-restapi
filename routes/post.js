84038f4227bd9ad20087bb1e84f219bb41a5801c
rizviroshan10 <rizviroshan10@mhs.mdp.ac.id>
2023-05-25 13:19:49 +0700

    add route post,get,put,delete

--- routes/post.js
+++ routes/post.js
@@ -2,15 +2,61 @@ const express = require('express')
 const router = express.Router()
 const Post = require('../models/post')
 
-router.post('/', async(req, res) => {
-    try{
+router.get('/', async (req, res) => {
+    try {
         const post = await Post.find()
         res.json(post)
-    }catch (error) {
+    } catch (error) {
         res.json({
             message: error
         })
     }
 })
 
-module.exports =router
+router.post('/', async (req, res) => {
+    const inputPost = new Post({
+        content: req.body.content,
+        user_id: req.body.user_id,
+        username: req.body.username
+    })
+
+    try {
+        const post = await inputPost.save()
+        res.json(post)
+    } catch (error) {
+        res.json({
+            message: error
+        })
+    }
+})
+
+router.put('/:postId', async (req, res) => {
+    const data ={
+        content: req.body.content,
+    }
+    try{
+        const post = await this.post.updateOne({
+            _id: req.params.postId
+        }, data)
+        res.json(post)
+    } catch (error){
+        res.json({
+            message: error
+        })
+    }
+})
+
+router.delete('/:postId', async (req,res) => {
+    try{
+        const post = await Post.deleteOne({
+            _id: req.params.postId
+        })
+        res.json(post)
+    } catch (error){
+        res.json({
+            message: error
+        })
+    }
+})
+
+module.exports = router
